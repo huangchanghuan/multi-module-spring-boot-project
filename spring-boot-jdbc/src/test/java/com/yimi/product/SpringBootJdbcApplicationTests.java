c687a73be437294ba9d647c193d11a0f473a99f7
peter <2464375449@qq.com>
2018-02-20 16:54:49 +0800

    添加爬虫测试mainWebClientModule

--- spring-boot-jdbc/src/test/java/com/yimi/product/SpringBootJdbcApplicationTests.java
+++ spring-boot-jdbc/src/test/java/com/yimi/product/SpringBootJdbcApplicationTests.java
@@ -1,10 +1,7 @@
 package com.yimi.product;
 
-import com.yimi.product.module.user.dao.UserDao;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,15 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringBootJdbcApplicationTests {
 
-	@Autowired
-	private UserDao userDao;
 	@Test
 	public void contextLoads() {
-		//插入两个用户
-		userDao.addUser("奥巴马",1);
-		userDao.addUser("Jack",1);
-		//数据库应该有2个用户,所以这里不通过
-		Assert.assertEquals(3,userDao.getAllUsers().intValue());
+
 	}
 
 }
